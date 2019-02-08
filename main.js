ce541bcd37ddec8db2192a88fba3c22406843974
Alex Burley <alexburley3@gmail.com>
2019-02-08 22:07:28 +0000

    express server

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const quotes = require("./quotes.json");
 const quotesArray = Object.keys(quotes).map(i => quotes[i]);
-// setTimeout(() => {
-// }, 1000*60*60*24);
-
 const generateQuote = () => {
-    console.log(quotesArray[Math.floor(Math.random()*quotesArray.length)]);
+    let randomQuote = quotesArray[Math.floor(Math.random()*quotesArray.length)]
+    quoteOfTheDay = randomQuote;
+    console.log(randomQuote);
     setTimeout(() => {
         generateQuote();
-    }, 1000*5);
+    }, 1000*60*60*24);
 };
 
 generateQuote();
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send(`${quoteOfTheDay.quoteText} - ${quoteOfTheDay.quoteAuthor}`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
