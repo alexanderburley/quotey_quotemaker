9b1453c58d648ba39f5d1fcf5362bbec66d46ec4
Alex Burley <alexburley3@gmail.com>
2019-02-10 14:25:02 +0000

    Extract out and start test suite

--- main.js
+++ main.js
@@ -1,20 +1,8 @@
-const quotes = require("./quotes.json");
-const quotesArray = Object.keys(quotes).map(i => quotes[i]);
-let quoteOfTheDay;
-const generateQuote = () => {
-    let randomQuote = quotesArray[Math.floor(Math.random()*quotesArray.length)]
-    quoteOfTheDay = randomQuote;
-    console.log(randomQuote);
-    setTimeout(() => {
-        generateQuote();
-    }, 1000*60*60*24);
-};
 
-generateQuote();
 
 const port = process.env.PORT || 5000
 const express = require('express')
 const app = express()
-
+const quoteOfTheDay = require('./lib/quote.js').getQuoteOfTheDay();
 app.get('/', (req, res) => res.send(`${quoteOfTheDay.quoteText} - ${quoteOfTheDay.quoteAuthor}`));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
