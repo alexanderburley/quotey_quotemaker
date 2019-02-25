3dd994f5a337ba2df279bcbe34979a2abe21901f
Alex Burley <alexburley3@gmail.com>
2019-02-25 22:37:52 +0000

    Get a birth/death date from wiki pedia

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "no-console": "warn",
+        "no-undef": "warn",
+        "no-unused-vars": "warn"
+    }
+};
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 
-
 const port = process.env.PORT || 5000
 const express = require('express')
 const app = express()
-const quoteOfTheDay = require('./lib/quote.js').getQuoteOfTheDay();
-app.get('/', (req, res) => res.send(`${quoteOfTheDay.quoteText} - ${quoteOfTheDay.quoteAuthor}`));
+const quoteOfTheDay = require('./lib/quoteBuilder.js').getQuoteOfTheDay();
+app.get('/', (req, res) => {
+  quoteOfTheDay.then((quote) => {
+    res.send(`${quote.text} - ${quote.author} - ${quote.lifespan}`)
+  })
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
