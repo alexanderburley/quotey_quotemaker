e512176468ba97422f3722421edb1900bf0ffa01
Alex Burley <alexburley3@gmail.com>
2019-05-06 18:03:48 +0100

    Serve react from express

--- main.js
+++ main.js
@@ -4,11 +4,28 @@ const port = process.env.PORT || 3001
 const express = require('express')
 const app = express()
 const quoteOfTheDay = require('./lib/quoteBuilder.js').getQuoteOfTheDay();
-app.get('/', (req, res) => {
-  quoteOfTheDay.then((quote) => {
-    res.send(`${quote.text} - ${quote.author} - ${quote.lifespan}`)
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
   })
-});
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
+// app.get('/', (req, res) => {
+//   quoteOfTheDay.then((quote) => {
+//     res.send(`${quote.text} - ${quote.author} - ${quote.lifespan}`)
+//   })
+// });
 
 app.get('/quote', (req, res) => {
   quoteOfTheDay.then(quote => res.send(quote));
