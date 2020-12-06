05ef0043407e3c415ee6f83173a227eabf7773e8
burlzad <alexburley3@live.co.uk>
2020-12-06 18:33:06 +0000

    Remove fe

--- main.js
+++ main.js
@@ -1,32 +1,12 @@
 /* eslint-disable no-undef */
 
-const port = process.env.PORT || 3001
-const path = require('path');
-const express = require('express')
-const app = express()
-const quoteOfTheDay = require('./lib/quoteBuilder.js').getQuoteOfTheDay();
+const port = process.env.PORT || 3001;
+const express = require("express");
+const app = express();
+const quoteOfTheDay = require("./lib/quoteBuilder.js").getQuoteOfTheDay();
 
-
-app.get('/quote', (req, res) => {
-  quoteOfTheDay.then(quote => res.send(quote));
-})
-
-//Static file declaration
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-//production mode
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
-  })
-}
-//build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-})
-
-
+app.get("/quote", (req, res) => {
+  quoteOfTheDay.then((quote) => res.send(quote));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
